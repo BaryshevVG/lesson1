d6da179d589bd2cda857a9966d7390fea35ddce9
Vadim Baryshev <vadik.barysheff@gmail.com>
2024-02-19 14:06:09 +0500

    add lesson2:task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 15;
+int b = 1;
+int c = 101;
+int d = 42;
+int e = 18;
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+if(d > max)
+{
+    max = d;
+}
+if(e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
